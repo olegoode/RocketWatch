a239cbb5182eb719d01220af722bb86763cd565c
Ole Goode <ole@artemisit.com>
2017-03-20 17:34:19 -0400

    LL via SSL

--- imports/api/index.js
+++ imports/api/index.js
@@ -1,4 +1,4 @@
-const baseURL = 'http://launchlibrary.net/1.2/';
+const baseURL = 'https://launchlibrary.net/1.2/';
 
 export const getLaunches = (num = 20) => {
   return fetch(`${baseURL}/launch/next/${num}`)
