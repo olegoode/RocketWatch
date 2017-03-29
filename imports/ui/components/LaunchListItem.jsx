5a7a4d60ce5773b5d5f164f47eae8b0e2315362e
Ole Goode <olegoode@gmail.com>
2017-03-29 19:23:26 -0400

    Launch list styling

--- imports/ui/components/LaunchListItem.jsx
+++ imports/ui/components/LaunchListItem.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
 
 const LaunchListItem = (props) => {
+  const missionDesc = props.missions.length > 0 ? props.missions[0].description : 'No description available.';
+
   return (
     <li className="item">
-      <div>
+      <div className="name">
         <p className="title">{props.name}</p>
         <p className="subtitle">{props.windowstart}</p>
       </div>
       <div className="meta">
-        <p><a alt={props.rocket.agencies[0].name + "website"} href={props.rocket.agencies[0].infoURL}>{props.rocket.agencies[0].name}</a></p>
+        <p><a alt={props.rocket.agencies[0].name + "website"} href={props.rocket.agencies[0].infoURL} target="_blank">{props.rocket.agencies[0].name}</a></p>
         <p>{props.location.name}</p>
+      </div>
+      <div className="description content">
+        <p>{missionDesc}</p>
       </div>
     </li>
   );
