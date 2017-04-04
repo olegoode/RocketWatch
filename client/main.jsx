94660fcc582069b99d1b9c011f9bc7be32263089
Ole Goode <olegoode@gmail.com>
2017-04-03 22:17:31 -0400

    Push Notifications/Service worker boilerplate

--- client/main.jsx
+++ client/main.jsx
@@ -6,4 +6,12 @@ import App from '../imports/ui/containers/App.jsx';
 
 Meteor.startup(() => {
   render(<App />, document.getElementById('root'));
+
+  if ('serviceWorker' in navigator && 'PushManager' in window) {
+    console.log('SW and PushManager are supported');
+    navigator.serviceWorker.register('js/worker.js')
+      .then((swReg) => {
+        console.log('serviceWorker is registered', swReg);
+      });
+  }
 });
---
+++ imports/lib/notifications.js
@@ -0,0 +1,1 @@
+// Notifications library for app
